a3adf0c665380e97ca4d80c7bfbd3983abc8fef6
soroush amini <soroush.araste@gmail.com>
2022-06-30 01:39:44 +0430

    initial commit

---
+++ ForecastWeather/ForecastWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastWeatherApp.swift
+//  ForecastWeather
+//
+//  Created by soroush amini araste on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct ForecastWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
