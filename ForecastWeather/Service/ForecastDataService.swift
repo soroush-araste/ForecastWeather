50061b76a757d4c208e6f97851657b93d706f10c
soroush amini <soroush.araste@gmail.com>
2022-06-30 15:21:31 +0430

    ForecastDataService has been added

---
+++ ForecastWeather/Service/ForecastDataService.swift
@@ -0,0 +1,36 @@
+//
+//  ForecastDataService.swift
+//  ForecastWeather
+//
+//  Created by soroush amini araste on 6/30/22.
+//
+
+import Foundation
+import Combine
+
+protocol ForecastDataProtocol {
+    func getForecast() -> AnyPublisher<Forecast,Error>
+}
+
+class ForecastDataService: ForecastDataProtocol {
+    
+    private var jsonDecoder: JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .useDefaultKeys
+        return decoder
+    }
+    
+    private let url: URL
+    
+    init(url: URL) {
+        self.url = url
+    }
+    
+    func getForecast() -> AnyPublisher<Forecast,Error> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map({$0.data})
+            .decode(type: Forecast.self, decoder: jsonDecoder)
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
