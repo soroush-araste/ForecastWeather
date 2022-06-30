88154477d4965ee285cd05441b53f6c9a8dbf3c5
soroush amini <soroush.araste@gmail.com>
2022-06-30 15:22:16 +0430

    injecting data service

--- ForecastWeather/ForecastWeatherApp.swift
+++ ForecastWeather/ForecastWeatherApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct ForecastWeatherApp: App {
+    
+    let dataService = ForecastDataService(url: URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=36.32277&lon=59.53649&exclude=current,minutly,hourly,alerts&appid=db9e0317c0efaa1bcd7111d4c063cd2a")!)
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(dataService: dataService)
         }
     }
 }
