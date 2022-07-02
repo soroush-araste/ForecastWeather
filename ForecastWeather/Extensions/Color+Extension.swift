b38cce4046b7c1adfc25668b92fc16a4828696c7
soroush amini <soroush.araste@gmail.com>
2022-07-02 10:08:43 +0430

    Create Color+Extension.swift

---
+++ ForecastWeather/Extensions/Color+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extension.swift
+//  ForecastWeather
+//
+//  Created by soroush amini araste on 7/1/22.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let searchBarColor = Color("searchBarColor")
+    let cardBgColor    = Color("cardbgColor")
+    let bgColor        = Color("bgColor")
+}
