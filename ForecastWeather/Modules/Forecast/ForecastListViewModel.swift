c30e6f52dc5893be0d3b73f0178a6882c573ddac
soroush amini <soroush.araste@gmail.com>
2022-07-04 18:44:27 +0430

    Update ForecastListViewModel.swift

--- ForecastWeather/Modules/Forecast/ForecastListViewModel.swift
+++ ForecastWeather/Modules/Forecast/ForecastListViewModel.swift
@@ -8,22 +8,28 @@
 import Foundation
 import Combine
 import CoreLocation
+import SwiftUI
 
 class ForecastListViewModel: ObservableObject  {
     
     @Published var forecastViewModelItems: [ForecastViewModel] = []
     @Published var searchCityName: String = ""
-    @Published var isLoading: Bool = true
+    @Published var isLoading: Bool = false
     @Published var city: String = "Paris"
+    @Published var showError: Bool = false
     
+    //@Published var bannerData: BannerModifier.BannerData = BannerModifier.BannerData(title: "", detail: "Notification text for the action you were trying to perform.", type: .Error)
+    var errorMessage: String = ""
     var currentURL: URL?
     var anyCancellable = Set<AnyCancellable>()
-    var dataService: ForecastDataService? = nil
+    var dataService: ForecastRemoteDataService? = nil
+    
+    var repository: ForecastRepository? = nil
     
     init() {
         let defaultURL = urlCreator()
-        loadData(url: defaultURL)
         getCityWeather()
+        getDataFromRepository(url: defaultURL)
     }
     
     func urlCreator(lat: Double = 48.857191, lon: Double = 2.352902) -> URL {
@@ -34,28 +40,34 @@ class ForecastListViewModel: ObservableObject  {
         return url
     }
     
-    func loadData(url: URL) {
+    func getDataFromRepository(url: URL) {
+        let dataService = ForecastRemoteDataService(url: url)
+        repository = ForecastRepository(remoteDataSource: dataService)
         isLoading = true
-        dataService = ForecastDataService(url: url)
-        dataService?.getForecast()
-            .sink { [weak self] receiveCompletion in
-                switch receiveCompletion{
+        repository?.get()
+            .sink(receiveCompletion: { [weak self] completion in
+                switch completion{
                 case .finished:
                     self?.isLoading = false
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    self?.isLoading = false
+                    //uncomment this line to show a banner for erro
+                    //self?.bannerData = BannerModifier.BannerData(title: "ERROR:", detail: error.localizedDescription, type: .Error)
+                    self?.errorMessage = error.localizedDescription
+                    self?.showError = true
                 }
-            } receiveValue: { [weak self] returnedItems in
+            },
+            receiveValue: { [weak self] returnedItems in
                 DispatchQueue.main.async {
-                    self?.forecastViewModelItems = returnedItems.daily.map({ ForecastViewModel(forecast: $0, current: returnedItems.current )})
+                    self?.forecastViewModelItems = returnedItems.daily.map({ForecastViewModel(dailyForecast: $0, current: returnedItems.current)})
                 }
-            }
+            })
             .store(in: &anyCancellable)
     }
-    
+        
     func refreshData() {
         guard let url = currentURL else { return }
-        loadData(url: url)
+        getDataFromRepository(url: url)
     }
     
     func getCityWeather() {
@@ -65,9 +77,8 @@ class ForecastListViewModel: ObservableObject  {
                 guard let self = self else { return }
                 self.getCityLocation(cityName: name) { lat, lon in
                     let url = self.urlCreator(lat: lat, lon: lon)
-                    self.loadData(url: url)
+                    self.getDataFromRepository(url: url)
                     print(name)
-                    
                 }
             }
             .store(in: &anyCancellable)
