08c8f76894f5fc570fa0e4a784e557758805ec6c
soroush amini <soroush.araste@gmail.com>
2022-07-04 18:44:22 +0430

    Update ForecastDetailView.swift

--- ForecastWeather/Modules/Forecast/ForecastDetailView.swift
+++ ForecastWeather/Modules/Forecast/ForecastDetailView.swift
@@ -36,9 +36,10 @@ struct ForecastDetailView_Previews: PreviewProvider {
     
     static var previews: some View {
         let forecastViewModel = ForecastViewModel(
-            forecast: Daily(date: Date(), temp: Temp(day: 10, min: 10, max: 10,night: 22), humidity: 10, weather: [], clouds: 10, pop: 10,uvIndex: 10, feelsLike: FeelsLike(day: 10, night: 10, eve: 10, morn: 10), pressure: 22, windSpeed: 10, sunrise: Date(), sunset: Date(), moonrise: Date()),
+            dailyForecast: DomainDaily(date: Date(), temp: DomainTemp(day: 10, min: 10, max: 10,night: 22), humidity: 10, weather: [],uvIndex: 10, feelsLike: DomainFeelsLike(day: 10, night: 10, eve: 10, morn: 10), pressure: 22, windSpeed: 10, sunrise: Date(), sunset: Date(), moonrise: Date()),
             
-            current: Current(dt: 10, sunrise: 10, sunset: 10, temp: 10, feelsLike: 10, pressure: 10, humidity: 10, dewPoint: 10, uvi: 10, clouds: 10, visibility: 10, windSpeed: 10, windDeg: 10, weather: []))
+            current: DomainCurrent(temp: 10, weather: []))
+        
         ForecastDetailView(forecastViewModel: forecastViewModel)
             .environmentObject(ForecastListViewModel())
     }
